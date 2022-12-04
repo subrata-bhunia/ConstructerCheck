6b77eb3432fd1ad14a4db2acb172e5417a2110c9
subrata-bhunia <eerrcfgy@gmail.com>
2022-12-04 21:50:14 +0530

    Add project files.

---
+++ ConstructerCheck/Trainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructerCheck
+{
+    class Trainer
+    {
+       public int empId;
+       public string empName;
+       public string tech;
+        public Trainer()
+        {
+            empId = 101;
+            empName = "Peter";
+            tech = "Dotnet";
+        }
+        public Trainer(int id,string name,string technology)
+        {
+            empId = id;
+            empName = name;
+            tech = technology;
+        }
+        public void show()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
